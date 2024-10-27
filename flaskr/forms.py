cfaeea20cd2315db3cbb57e45e538ca5a0fcefb7
Masoud Bagheri <161155114+bagherimasoud@users.noreply.github.com>
2024-10-27 12:47:37 +0330

    Delete flaskr/forms.py

--- flaskr/forms.py
+++
@@ -1,9 +0,0 @@
-# forms.py
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired
-
-class UserProfileForm(FlaskForm):
-    bio = TextAreaField('Bio')
-    location = StringField('Location', validators=[DataRequired()])
-    submit = SubmitField('Save')
